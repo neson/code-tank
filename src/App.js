55e28a9a037d34681e6f799d150e753f6a3852eb
Pokai Chang <pokaichang72@gmail.com>
2016-12-26 12:24:36 +0800

    排出初版前端畫面

--- src/App.js
+++ src/App.js
@@ -1,18 +1,67 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+
+import $ from 'jquery';
+window.jQuery = $;
+
+require('bootstrap/dist/js/bootstrap.js');
+require('bootstrap/dist/css/bootstrap.css');
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
+        <nav className="navbar navbar-default navbar-fixed-top">
+          <div className="container-fluid">
+            <div className="navbar-header">
+              <button type="button" className="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar" aria-expanded="false" aria-controls="navbar">
+                <span className="sr-only">Toggle navigation</span>
+                <span className="icon-bar"></span>
+                <span className="icon-bar"></span>
+                <span className="icon-bar"></span>
+              </button>
+              <a className="navbar-brand" href="#">Code Tank</a>
+            </div>
+            <div id="navbar" className="navbar-collapse collapse">
+              <ul className="nav navbar-nav">
+                <li className="active"><a href="#">Player</a></li>
+              </ul>
+            </div>
+          </div>
+        </nav>
+
+        <div className="container-fluid" style={{ width: '100%', height: '100%', paddingTop: '52px' }}>
+          <table style={{ width: '100%', height: '100%' }}>
+            <tr style={{ width: '100%', height: '50%' }}>
+              <td style={{ width: '50%', height: '50%' }}>
+                <h3>遊戲畫面</h3>
+                <pre>
+                  ...<br/>
+                  ...<br/>
+                  ...<br/>
+                  ...<br/>
+                  ...<br/>
+                </pre>
+              </td>
+              <td style={{ width: '50%', height: '50%' }}>
+                <textarea style={{ boxSizing: 'border-box', width: '100%', height: '100%' }}>
+                  // 初始地圖
+                </textarea>
+              </td>
+            </tr>
+            <tr style={{ width: '100%', height: '50%' }}>
+              <td style={{ width: '50%', height: '50%' }}>
+                <textarea style={{ boxSizing: 'border-box', width: '100%', height: '100%' }}>
+                  // P1 程式碼
+                </textarea>
+              </td>
+              <td style={{ width: '50%', height: '50%' }}>
+                <textarea style={{ boxSizing: 'border-box', width: '100%', height: '100%' }}>
+                  // P2 程式碼
+                </textarea>
+              </td>
+            </tr>
+          </table>
         </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
       </div>
     );
   }
